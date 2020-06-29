4361ba4e66e18d50c6148bc1c754f684d56d5c48
Kevin Anderson <kmichael419@hotmail.com>
2020-06-29 13:37:54 -0500

    added click counter

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 $(document).ready(onReady);
 
+let clickCount = 0;
+
 function onReady() {
   $('.js-btn-generate').on('click', makeBox);
 }
 
 function makeBox() {
+  clickCount++;
   $('.js-container').append(`
     <div class="box">
-      <button class="js-btn-yellow">Yellow</button>
+        <p>${clickCount}</p>
+        <button class="js-btn-yellow">Yellow</button>
     </div>
   `);
 }
